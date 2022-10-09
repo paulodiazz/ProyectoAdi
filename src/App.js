5f5ee81569d25c4304e00bdc78088a63d914a39c
Paulo Diaz <paulodiazg32@gmail.com>
2022-10-09 02:06:00 -0700

    Repaired commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { Route, Routes, HashRouter } from 'react-router-dom';
-
 import styles from './styles/header.module.css';
-import { ConnectButton } from '@rainbow-me/rainbowkit';
 
 import Home from "./pages/Home";
 import Leaderboard from "./pages/Leaderboard";
 import Hunt from "./pages/Hunt";
-import Challenge from "./pages/Challenge";
 
 import {handleConnectWallet} from './wallet/walletUtils';
 import { useAccount, useSignMessage } from 'wagmi';
@@ -32,7 +29,7 @@ function App() {
       const signature = await signMessageAsync({ message: challenge });
       const accessToken = await authenticate(account, signature);
       console.log({ accessToken });
-      window.sessionStorage.setItem('lensAccessToken', accessToken);
+      window.sessionStorage.setItem('accessToken', accessToken);
       setSignedIn(true);
     } catch (error) {
       console.error(error);
@@ -41,32 +38,47 @@ function App() {
   };
 
   return (
-    <div>
-      <div className={styles.header}>
-
-      {account ?
-              <button class="grey" onClick={async ()=>{
+    <div className="o-hidden">
+      <div className="row pt-3" style={{backgroundColor: "#391C00"}}>
+        <div className="col-6 d-flex justify-content-center">
+          {account ?
+            <button className="btn-browngold"
+              onClick={async ()=>{
                 await web3modal.clearCachedProvider();
                 setAccount(undefined);
                 setProvider();
-              }}>{account}</button>
-              :
-                  <button class="grey" onClick={
-                () => {
+              }}>
+                <p className="px-2">
+                  {account.slice(0, 12).concat('...')}
+                </p>
+            </button>
+          :
+            <button className="btn-browngold"
+            style={{maxWidth: "160px"}}
+              onClick={ () => {
                 handleConnectWallet({
-                  setAccount,
-                  setWeb3modal,
-                  setProvider,
-                  setProviderexp,
-                  setWalletConnection
-                })
-                }}>CONNECT WALLET</button>
-              }
-      {!signedIn && (
-        <button onClick={signIn} margintop='2'>
-          Login with Lens
-        </button>
-      )}
+                setAccount,
+                setWeb3modal,
+                setProvider,
+                setProviderexp,
+                setWalletConnection
+            })}}>
+              <p className="px-2">
+                CONNECT WALLET
+              </p>
+            </button>
+          }
+        </div>
+        <div className="col-6 d-flex justify-content-center">
+          {!signedIn && (
+            <button className="btn-browngold"
+            style={{maxWidth: "160px"}} onClick={signIn}>
+              <p className="px-2 text-uppercase">
+                Login with Lens
+              </p>
+            </button>
+          )}
+        </div>
       </div>
       <div>
         <div>
@@ -79,9 +91,6 @@ function App() {
             </Routes>
             <Routes>
               <Route path="/leaderboard" element={<Leaderboard />}/>
-            </Routes>
-            <Routes>
-              <Route path="/challenge/:challengeId" element={<Challenge />}/>
             </Routes>
           </HashRouter>
         </div>
